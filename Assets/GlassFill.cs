6a3c903f38863358f60ab349512e688b1a0660c8
Bartosz Kluchciński <bkluchcinski@gmail.com>
2017-08-01 19:54:28 +0200

    added foam particle when over poured

--- Assets/GlassFill.cs
+++ Assets/GlassFill.cs
@@ -6,16 +6,19 @@ public class GlassFill : MonoBehaviour
 {
     [SerializeField]
     private GameObject foam, moreFoam, overFoam;
+
+    [SerializeField] private ParticleSystem foamParticle;
+
     private float fillLevel = 0;
 
     private Vector3 startFoamPos;
     private float timer = 0;
 
+
     [SerializeField] private SpriteRenderer sr;
 	void Start ()
 	{
 	    startFoamPos = foam.transform.localPosition;
-
         startFoamPos.y = 0;
 	    foam.transform.position = startFoamPos;
 	    moreFoam.transform.position = startFoamPos;
@@ -26,6 +29,18 @@ public class GlassFill : MonoBehaviour
     void Update()
     {
         sr.gameObject.transform.localScale = new Vector3(1, Mathf.Clamp01(fillLevel), 1);
+
+
+        if (fillLevel >= 1)
+        {
+            overFoam.transform.localPosition = Vector3.Lerp(startFoamPos, new Vector3(startFoamPos.x, startFoamPos.y + 62, startFoamPos.z), fillLevel- 0.05f);
+            if (fillLevel >= 1.1f && !foamParticle.isPlaying)
+            {
+                foamParticle.Play();
+            }
+        }
+        //if(updatePosition)
+        //    overFoam.transform.position = foam.transform.position;
     }
 
     public void StartFill(float fillSpeed)
@@ -33,6 +48,7 @@ public class GlassFill : MonoBehaviour
         fillLevel = fillLevel + Time.deltaTime * fillSpeed;
         foam.transform.localPosition = Vector3.Lerp(startFoamPos, new Vector3(startFoamPos.x, startFoamPos.y + 59, startFoamPos.z), fillLevel);
         moreFoam.transform.localPosition = Vector3.Lerp(startFoamPos, new Vector3(startFoamPos.x, startFoamPos.y + 52, startFoamPos.z), fillLevel);
+        timer = 0;
     }
 
     public float? EndFill(float endSpeed, float endTime)
@@ -40,6 +56,8 @@ public class GlassFill : MonoBehaviour
         if (timer <= endTime)
         {
             fillLevel = fillLevel + Time.deltaTime * endSpeed;
+            foam.transform.localPosition = Vector3.Lerp(startFoamPos, new Vector3(startFoamPos.x, startFoamPos.y + 59, startFoamPos.z), fillLevel);
+            moreFoam.transform.localPosition = Vector3.Lerp(startFoamPos, new Vector3(startFoamPos.x, startFoamPos.y + 52, startFoamPos.z), fillLevel);
             timer += Time.deltaTime;
             return null;
         }
