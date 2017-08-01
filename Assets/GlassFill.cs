ce152e2e6c41b7042cd857b12ebef8fd999e399e
Bartosz Kluchciński <bkluchcinski@gmail.com>
2017-08-01 16:21:09 +0200

    glass filling and scene

---
+++ Assets/GlassFill.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GlassFill : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject foam, moreFoam;
+    [SerializeField]
+    private float fillSpeed;
+    private float fillLevel = 0;
+
+    private Vector3 startFoamPos;
+
+    [SerializeField] private SpriteRenderer sr;
+	// Use this for initialization
+	void Start ()
+	{
+	    startFoamPos = foam.transform.localPosition;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Fill();
+	    foam.transform.localPosition = Vector3.Lerp(startFoamPos, new Vector3(startFoamPos.x,startFoamPos.y + 62,startFoamPos.z), fillLevel);
+	    moreFoam.transform.localPosition = Vector3.Lerp(startFoamPos, new Vector3(startFoamPos.x, startFoamPos.y + 55, startFoamPos.z), fillLevel);
+        sr.gameObject.transform.localScale = new Vector3(1,fillLevel,1);
+	}
+
+    public void Fill()
+    {
+        fillLevel = fillLevel +Time.deltaTime * fillSpeed;
+    }
+}
