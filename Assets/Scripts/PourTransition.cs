696172fb4b1c4bff01283420add1fe0179349670
RadekOlenski <radek.olenski@gmail.com>
2017-08-01 13:12:57 +0200

    Add glass

---
+++ Assets/Scripts/PourTransition.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+public class PourTransition : MonoBehaviour
+{
+    #region Properties
+
+    public GameObject Glass;
+
+    #endregion
+
+    #region Unity Methods
+
+    public void Start()
+    {
+
+    }
+
+    public void FixedUpdate()
+    {
+        Vector3 xAxis = new Vector3(Input.acceleration.x, 0f, 0f);
+
+        float angle = Vector3.Angle(Physics.gravity, xAxis);
+
+        Glass.transform.position += new Vector3(0.1f * angle, 0f, 0f);
+    }
+
+    #endregion
+
+}
